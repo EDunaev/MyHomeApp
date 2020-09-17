0b7b1224364c61f3a8af51d027672b9c3bcd53f1
edunaev <dunaeve@yahoo.com>
2020-09-17 20:12:40 +0200

    visual changes in ui, delete and change buttons are moved into table cells

--- src/app/content-table/content-table.component.ts
+++ src/app/content-table/content-table.component.ts
@@ -1,51 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WindelschichtService } from '../services/windelschicht.service';
 import { SchichtTO } from '../TOs/SchichtTO';
+import * as _ from 'lodash'; 
 
 @Component({
   selector: 'app-content-table',
   templateUrl: './content-table.component.html',
   styleUrls: ['./content-table.component.css']
 })
-export class ContentTableComponent  {
+export class ContentTableComponent implements OnInit{
 
-
+  isOpen: Boolean;
   posts: SchichtTO[] = [];
+  selectedPosts: SchichtTO[] = [];
   selectedSchicht: SchichtTO;
-  constructor(private windelschichtService: WindelschichtService) { 
+
+  constructor(private windelschichtService: WindelschichtService) { }
+  
+  
+  ngOnInit(): void {
     this.findAllShifts();
   }
 
   findAllShifts() {
-    this.windelschichtService.findAllShifts().subscribe(data => this.parseData(data));
+    this.windelschichtService.findAllShifts().subscribe(data => {
+      this.parseDataList(data);
+      this.refreshTableView();
+    });
   }
 
   papa() {
-    this.windelschichtService.papa().subscribe(data => this.parseData(data));
+    this.windelschichtService.papa().subscribe(data => {
+      this.parseData(data);
+      this.refreshTableView();
+    });
   }
   mama() {
-    this.windelschichtService.mama().subscribe(data => this.parseData(data));
+    this.windelschichtService.mama().subscribe(data => {
+      this.parseData(data);
+      this.refreshTableView();
+    });
   }
 
   onDelete(post: SchichtTO) {
-    this.windelschichtService.delete(post.id).subscribe(res => {
-      var result: Boolean;
-      res === 'true' ? result = true: result = false; 
-      if(result) {
-        console.log("Entity " + post.id + " was deleted");
-        var index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-      }
-      else{
-        console.log("Entity " + post.id + " was not deleted");
-      }
+    this.windelschichtService.delete(post.id).subscribe(
+      res => {
+        if(res) {
+          console.log("Entity " + post.id + " was deleted");
+          var index = this.posts.indexOf(post);
+          this.posts.splice(index, 1);
+          this.refreshTableView();
+        }
+        else{
+          console.log("Entity " + post.id + " was not deleted");
+        }
+      },
+      error => console.log("Error: ", error)
+      );
+  }
+
+  onChange(post: SchichtTO) {
+    console.log("tbd");
+    
+  }
+
+  parseDataList(data: any) {
+    data.forEach((element: any) => {
+      this.parseData(element);
     });
   }
 
   parseData(data: any) {
-    data.forEach(element => {
-      this.posts.push(SchichtTO.create(element));
-    });
+      this.posts.push(SchichtTO.create(data));
   }
 
   selectedPost(selectedSchicht: SchichtTO) {
@@ -54,6 +80,20 @@ export class ContentTableComponent  {
 
   isPostSelected(id: number) {
     return this.selectedSchicht && this.selectedSchicht.id === id;
+  }
+
+  collapseExpand() {
+    this.isOpen = !this.isOpen;
+    	this.refreshTableView();
+  }
+
+  refreshTableView() {
+    if(this.isOpen) {
+      this.selectedPosts = this.posts;
+    }
+    else {
+      this.selectedPosts = _.takeRight(this.posts, 5);
+    }
   }
 
 }
