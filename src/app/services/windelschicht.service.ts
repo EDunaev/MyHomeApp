021e33216d67dec16b99722daad584b890cc56ce
edunaev <dunaeve@yahoo.com>
2020-09-28 11:21:30 +0200

    update from 8 to 10

--- src/app/services/windelschicht.service.ts
+++ src/app/services/windelschicht.service.ts
@@ -11,33 +11,18 @@ export class WindelschichtService {
   uri = environment.server;
   constructor(private http: HttpClient) { }
 
-
+  //todo add error handling 
   findAllShifts() {
-    return this.http.get(this.uri + '/api/windel/')
-      .pipe(map(response => {
-        console.log(response);
-        console.log(response);
-        
-        return response;
-      }))
+    return this.http.get(this.uri + '/api/windel/').pipe();
   }
 
   papa() {
-    return this.http.get(this.uri + '/api/windel/1')
-    .pipe(map(response => {
-      return response;
-    }))
+    return this.http.get(this.uri + '/api/windel/1').pipe();
   }
   mama() {
-    return this.http.get(this.uri + '/api/windel/2')
-    .pipe(map(response => {
-      return response;
-    }))
+    return this.http.get(this.uri + '/api/windel/2').pipe();
   }
   delete(id: Number) {
-    return this.http.delete(this.uri + '/api/windel/' + id)
-    .pipe(map(response => {
-      return response;
-    }))
+    return this.http.delete(this.uri + '/api/windel/' + id).pipe();
   }
 }
